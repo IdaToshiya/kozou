fee1bef50376b1b353e327f832c14fbe3d95798b
7d02 <idakohaku1925@gmail.com>
2024-11-13 10:07:39 +0900

    beannnnn

--- 貯蔵小僧帳王/src/main/java/bean/Bean.java
+++ 貯蔵小僧帳王/src/main/java/bean/Bean.java
@@ -9,24 +9,18 @@ public class Bean implements java.io.Serializable {
 	private String password;
 	private String prefectures;
 	private String sex;
-	private int age;
 	private Date dateOfBirth;
 	private String emailAddress;
 	private int productNumber;
-	private String productName;
-	private int productOfRegistrations;
 	private int stock;
 	private Date startUsing;
-	private Date endOfUse;
+	private Date endUse;
 	private int periodDenominator;
 	private int periodNumerator;
-	private int activeNomber;
-
-
-
-
-
-
+	private int activeNumber;
+	private String productName;
+	private int categoryNumber;
+	private int numberOfRegistrations;
 	
 	public String getNickname() {
 		return nickname;
@@ -36,26 +30,62 @@ public class Bean implements java.io.Serializable {
 		return password;
 	}
 	
-	public String getEmailAddress() {
-		return emailAddress;
+	public String getPrefectures() {
+		return prefectures;
 	}
 	
 	public String getSex() {
 		return sex;
 	}
 	
-	public int getAge() {
-		return age;
-	}
-	
 	public Date getDateOfBirth() {
 		return dateOfBirth;
 	}
 	
-	public String getPrefectures() {
-		return prefectures;
+	public String getEmailAddress() {
+		return emailAddress;
 	}
 	
+	public int getProductNumber() {
+		return productNumber;
+	}
+	
+	public int getStock() {
+		return stock;
+	}
+	
+	public Date getstartUsing() {
+		return startUsing;
+	}
+	
+	public Date getEndUse() {
+		return endUse;
+	}
+	
+	public int getPeriodDenominator() {
+		return periodDenominator;
+	}
+	
+	public int getPeriodNumerator() {
+		return periodNumerator;
+	}
+	
+	public int getActiveNumber() {
+		return activeNumber;
+	}
+	
+	public String getProductName() {
+		return productName;
+	}
+	
+	public int getCategoryNumber() {
+		return categoryNumber;
+	}
+	
+	public int getNumberOfRegistrations() {
+		return numberOfRegistrations;
+	}
+//	------------------------------------------------------------------------------------
 	public void setNickname(String nickname) {
 		this.nickname=nickname;
 	}
@@ -72,15 +102,51 @@ public class Bean implements java.io.Serializable {
 		this.sex=sex;
 	}
 	
-	public void setAge(int age) {
-		this.age=age;
-	}
-	
 	public void setDateOfBirth(Date dateOfBirth) {
 		this.dateOfBirth=dateOfBirth;
 	}
 	
 	public void setEmailAddress(String emailAddress) {
 		this.emailAddress=emailAddress;
+	}
+	
+	public void setProductNumber(int productNumber) {
+		this.productNumber=productNumber;
+	}
+	
+	public void setStock(int stock) {
+		this.stock=stock;
+	}
+	
+	public void setStartUsing(Date startUsing) {
+		this.startUsing=startUsing;
+	}
+	
+	public void setEndUse(Date endUse) {
+		this.endUse=endUse;
+	}
+	
+	public void setPeriodDenominator(int periodDenominator) {
+		this.periodDenominator=periodDenominator;
+	}
+	
+	public void setPeriodNumerator(int periodNumerator) {
+		this.periodNumerator=periodNumerator;
+	}
+	
+	public void setActiveNumber(int activeNumber) {
+		this.activeNumber=activeNumber;
+	}
+	
+	public void setProductName(String productName) {
+		this.productName=productName;
+	}
+	
+	public void setCategoryNumber(int categoryNumber) {
+		this.categoryNumber=categoryNumber;
+	}
+	
+	public void setNumberOfRegistrations(int numberOfRegistrations) {
+		this.numberOfRegistrations=numberOfRegistrations;
 	}
 }
